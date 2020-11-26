cc327dd8153243939a7760eb0eb32e1bb5976ebb
yelzh <11l.yelzhank@gmail.com>
2020-11-26 23:45:06 +0600

    7 и 8 урок

---
+++ app/src/main/java/com/example/weatherappapi/CityListFragment.java
@@ -0,0 +1,21 @@
+package com.example.weatherappapi;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.ListFragment;
+
+public class CityListFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_city_list, container, false);
+    }
+}
--- app/src/main/java/com/example/weatherappapi/MainActivity.java
+++ app/src/main/java/com/example/weatherappapi/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.weatherappapi;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.drawable.Icon;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Parcelable;
+import android.os.PersistableBundle;
+import android.provider.SyncStateContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +21,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.weatherappapi.model.Main;
 import com.example.weatherappapi.model.Weather;
 import com.example.weatherappapi.model.WeatherRequest;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -22,6 +32,7 @@ import com.google.gson.Gson;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.Serializable;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.regex.Pattern;
@@ -30,22 +41,17 @@ import java.util.stream.Collectors;
 import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String CITY_KEY = "CITY";
+    private boolean isExistWeather;  // Можно ли расположить рядом фрагмент с погодой
     private static final String TAG = "WEATHER";
-
-    // Регулярные выражения позволяют проверить на соответствие шаблону
-    // Это имя. Первая буква большая латинская, остальные маленькие латинские
-    Pattern checkLogin = Pattern.compile("^[A-Z][a-z]{2,}$");
-
+    private Pattern checkLogin = Pattern.compile("^[A-Z][a-z]{2,}$"); // Регулярные выражения позволяют проверить на соответствие шаблону
     private TextInputEditText cityInput;
-    private EditText city;
-    private EditText temperature;
-    private EditText pressure;
-    private EditText humidity;
-    private EditText windSpeed;
-//    private ImageView weatherIcon;
     private FloatingActionButton floatingActionButton;
-
+    public EditText city;
+    public EditText temperature;
+    public EditText pressure;
+    public EditText humidity;
+    public EditText windSpeed;
     private static final String WEATHER_URL_PART_1 = "https://api.openweathermap.org/data/2.5/weather?q=";
     private static final String WEATHER_URL_PART_3 = ",&appid=";
 
@@ -54,7 +60,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -63,94 +68,119 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void init() {
-        cityInput = findViewById(R.id.textInputCity);
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
+
+    @Override
+    public void onRestoreInstanceState(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onRestoreInstanceState(savedInstanceState, persistentState);
+    }
+
+    public void init() {
         city = findViewById(R.id.textCity);
         temperature = findViewById(R.id.textTemprature);
         pressure = findViewById(R.id.textPressure);
         humidity = findViewById(R.id.textHumidity);
         windSpeed = findViewById(R.id.textWindspeed);
+        cityInput = findViewById(R.id.textInputCity);
         floatingActionButton = findViewById(R.id.floatingActionButton);
         Button search = findViewById(R.id.search);
-        search.setOnClickListener(clickListener);
+        isExistWeather = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;  // Определение, можно ли будет расположить рядом герб в другом фрагменте
     }
 
-    final View.OnClickListener clickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            try {
-                String WEATHER_URL_PART_2 = cityInput.getText().toString();
+    // Обработка нажатия кнопки
+    public void clickOnSearchButton(View view) {
+        validate(cityInput, checkLogin, "Это не город!");
+        String cityName = cityInput.getText().toString();
+        Toast.makeText(getApplicationContext(), cityName, Toast.LENGTH_SHORT).show();
+        getDetails(cityName);
+    }
 
-                final URL uri = new URL(WEATHER_URL_PART_1 + WEATHER_URL_PART_2 + WEATHER_URL_PART_3 + BuildConfig.WEATHER_API_KEY);
+    public void getDetails(String city) {
+        try {
+            String WEATHER_URL_PART_2 = city;
+            final URL uri = new URL(WEATHER_URL_PART_1 + WEATHER_URL_PART_2 + WEATHER_URL_PART_3 + BuildConfig.WEATHER_API_KEY);
+            Snackbar.make(cityInput, "Вы ищите погоду для города: " + WEATHER_URL_PART_2, Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            new Thread(new Runnable() {
+                public void run() {
+                    HttpsURLConnection urlConnection = null;
+                    try {
+                        urlConnection = (HttpsURLConnection) uri.openConnection();
+                        urlConnection.setRequestMethod("GET"); // установка метода получения данных -GET
+                        urlConnection.setReadTimeout(10000); // установка таймаута - 10 000 миллисекунд
+                        BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream())); // читаем  данные в поток
+                        String result = getLines(in);
 
-                new Thread(new Runnable() {
-                    public void run() {
-                        HttpsURLConnection urlConnection = null;
-                        try {
-                            urlConnection = (HttpsURLConnection) uri.openConnection();
-                            urlConnection.setRequestMethod("GET"); // установка метода получения данных -GET
-                            urlConnection.setReadTimeout(10000); // установка таймаута - 10 000 миллисекунд
-                            BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream())); // читаем  данные в поток
-                            String result = getLines(in);
-                            // преобразование данных запроса в модель
-                            Gson gson = new Gson();
-                            final WeatherRequest weatherRequest = gson.fromJson(result, WeatherRequest.class);
+                        // преобразование данных запроса в модель
+                        Gson gson = new Gson();
+                        final WeatherRequest weatherRequest = gson.fromJson(result, WeatherRequest.class);
 
-                            // Возвращаемся к основному потоку
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (isExistWeather) {
                                     displayWeather(weatherRequest);
+                                } else {
+                                    // Если нельзя вывести погоду рядом, откроем вторую activity
+                                    Intent intent = new Intent();
+                                    intent.setClass(MainActivity.this, CityActivity.class);
+                                    intent.putExtra("city", weatherRequest.getName());
+                                    intent.putExtra("temperature", weatherRequest.getMain().getTemp());
+                                    intent.putExtra("pressure", weatherRequest.getMain().getPressure());
+                                    intent.putExtra("humidity", weatherRequest.getMain().getHumidity());
+                                    intent.putExtra("windSpeed", weatherRequest.getWind().getSpeed());
+                                    startActivity(intent);
                                 }
-                            });
-                        } catch (Exception e) {
-                            Log.e(TAG, "Fail connection", e);
-                            e.printStackTrace();
-                        } finally {
-                            if (null != urlConnection) {
-                                urlConnection.disconnect();
                             }
+                        });
+                    } catch (Exception e) {
+                        Log.e(TAG, "Fail connection", e);
+                        e.printStackTrace();
+                    } finally {
+                        if (null != urlConnection) {
+                            urlConnection.disconnect();
                         }
                     }
-                }).start();
-            } catch (MalformedURLException e) {
-                Log.e(TAG, "Fail URI", e);
-                e.printStackTrace();
-            }
-            validate(cityInput, checkLogin, "Это не город!");
+                }
+            }).start();
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "Fail URI", e);
+            e.printStackTrace();
         }
+    }
 
-        private String getLines(BufferedReader in) {
-            return in.lines().collect(Collectors.joining("\n"));
+    private void displayWeather(WeatherRequest weatherRequest) {
+        city.setText(weatherRequest.getName());
+        temperature.setText(String.format("%f2", weatherRequest.getMain().getTemp()));
+        pressure.setText(String.format("%d", weatherRequest.getMain().getPressure()));
+        humidity.setText(String.format("%d", weatherRequest.getMain().getHumidity()));
+        windSpeed.setText(String.format("%d", weatherRequest.getWind().getSpeed()));
+    }
+
+    private String getLines(BufferedReader in) {
+        return in.lines().collect(Collectors.joining("\n"));
+    }
+
+    // Валидация
+    private void validate(TextView tv, Pattern check, String message) {
+        String value = tv.getText().toString();
+        if (check.matcher(value).matches()) {    // Проверим на основе регулярных выражений
+            hideError(tv);
+        } else {
+            showError(tv, message);
         }
+    }
 
-        private void displayWeather(WeatherRequest weatherRequest) {
-            city.setText(weatherRequest.getName());
-            temperature.setText(String.format("%f2", weatherRequest.getMain().getTemp()));
-            pressure.setText(String.format("%d", weatherRequest.getMain().getPressure()));
-            humidity.setText(String.format("%d", weatherRequest.getMain().getHumidity()));
-            windSpeed.setText(String.format("%d", weatherRequest.getWind().getSpeed()));
-        }
+    // Показать ошибку
+    private void showError(TextView view, String message) {
+        view.setError(message);
+    }
 
-        // Валидация
-        private void validate(TextView tv, Pattern check, String message) {
-            String value = tv.getText().toString();
-            if (check.matcher(value).matches()) {    // Проверим на основе регулярных выражений
-                hideError(tv);
-            } else {
-                showError(tv, message);
-            }
-        }
+    // спрятать ошибку
+    private void hideError(TextView view) {
+        view.setError(null);
+    }
 
-        // Показать ошибку
-        private void showError(TextView view, String message) {
-            view.setError(message);
-        }
-
-        // спрятать ошибку
-        private void hideError(TextView view) {
-            view.setError(null);
-        }
-
-    };
 }
